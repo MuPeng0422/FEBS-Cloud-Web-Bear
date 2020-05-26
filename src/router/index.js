b7ee8b648c6bb6f88eefc383e15ae5b79bf25ce1
mupeng <513991873@qq.com>
2020-05-26 19:58:08 +0800

    完成首页样式

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,112 @@
 import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Router from 'vue-router'
 
-Vue.use(VueRouter)
+Vue.use(Router)
 
-  const routes = [
+export const constantRouterMap = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Index',
+    component: () => import('@/views/Index'),
+    redirect: '/home',
+    children: [
+      {
+        path: 'home',
+        name: 'home',
+        component: () => import('@/views/home/home')
+      }
+      // {
+      //   path: '/us',
+      //   name: 'us',
+      //   component: () => import('@/views/us/us')
+      // },
+      // {
+      //   path: '/login',
+      //   name: 'login',
+      //   component: () => import('@/views/login/login')
+      // },
+      // {
+      //   path: '/register',
+      //   name: 'register',
+      //   component: () => import('@/views/login/login')
+      // },
+      // {
+      //   path: '/practices',
+      //   name: 'practices',
+      //   component: () => import('@/views/exam/practices')
+      // },
+      // {
+      //   path: '/exams',
+      //   name: 'exams',
+      //   component: () => import('@/views/exam/exams')
+      // },
+      // {
+      //   path: '/start/:id',
+      //   name: 'start',
+      //   component: () => import('@/views/exam/startExam')
+      // },
+      // {
+      //   path: '/exam-record',
+      //   name: 'exam-record',
+      //   component: () => import('@/views/exam/examRecords')
+      // },
+      // {
+      //   path: '/knowledge',
+      //   name: 'knowledge',
+      //   component: () => import('@/views/exam/knowledge')
+      // },
+      // {
+      //   path: '/practices',
+      //   name: 'practices',
+      //   component: () => import('@/views/exam/practices')
+      // },
+      // {
+      //   path: '/courses',
+      //   name: 'courses',
+      //   component: () => import('@/views/exam/courses')
+      // },
+      // {
+      //   path: '/course-details',
+      //   name: 'course-details',
+      //   component: () => import('@/views/exam/courseDetails')
+      // },
+      // {
+      //   path: '/account',
+      //   name: 'account',
+      //   component: () => import('@/views/personal/account')
+      // },
+      // {
+      //   path: '/password',
+      //   name: 'password',
+      //   component: () => import('@/views/personal/password')
+      // },
+      // {
+      //   path: '/incorrect',
+      //   name: 'incorrect',
+      //   component: () => import('@/views/exam/incorrect')
+      // },
+      // {
+      //   path: '/reset-password',
+      //   name: 'reset-password',
+      //   component: () => import('@/views/personal/resetPassword')
+      // }
+    ]
   },
+  // {
+  //   path: '/mobile',
+  //   name: 'Mobile',
+  //   component: () => import('@/views/mobile/Index')
+  // },
+  // {
+  //   path: '/mobile-v2',
+  //   name: 'MobileV2',
+  //   component: () => import('@/views/mobileV2/Index')
+  // },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '*',
+    redirect: '/home'
   }
 ]
-
-const router = new VueRouter({
-  mode: 'history',
-  base: process.env.BASE_URL,
-  routes
+export default new Router({
+  routes: constantRouterMap
 })
-
-export default router
